a84253ed593f96c4259a9352edaa9cd1eee2e963
lucy berger <lucyblueglass@gmail.com>
2016-06-20 20:59:36 -0700

    starts js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var suits = ["of hearts", "of diamonds", "of spades", "of clubs"];
+
+var cards = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+
+var full deck = [];
+suits.forEach(function(suit){
+  cards.forEach(function(card){
+    take cards add to suits and push to full deck
+  });
+});
